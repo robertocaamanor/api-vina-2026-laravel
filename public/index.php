9eac0edef5ff2c6f1c7ab7cf7068fc053ed83ad5
rabertcaamano <raristides.caamano@gmail.com>
2026-01-06 20:34:38 -0300

    Downgrade a Laravel 9

--- public/index.php
+++ public/index.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
+
+use Illuminate\Contracts\Http\Kernel;
 
 define('LARAVEL_START', microtime(true));
 
@@ -14,7 +15,14 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-/** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
