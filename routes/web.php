460260e16fda92f2d099c47663a48b04d253e1ec
rabertcaamano <raristides.caamano@gmail.com>
2026-01-06 20:58:51 -0300

    Implementación de SwaggerUI

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/docs', function () {
+    return view('docs');
+});
+
+Route::get('/openapi.yaml', function () {
+    $path = base_path('openapi.yaml');
+
+    abort_unless(File::exists($path), 404);
+
+    return response(File::get($path), 200)
+        ->header('Content-Type', 'application/yaml; charset=UTF-8');
 });
