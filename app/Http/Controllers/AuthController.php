a74be824198bda1c96b2904f3eba38603fbded1b
rabertcaamano <raristides.caamano@gmail.com>
2026-01-06 21:20:54 -0300

    Implementación de JWT

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required', 'string'],
+        ]);
+
+        $token = Auth::guard('api')->attempt($credentials);
+
+        if (! $token) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function me()
+    {
+        return response()->json(Auth::guard('api')->user());
+    }
+
+    public function logout()
+    {
+        Auth::guard('api')->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    public function refresh()
+    {
+        return $this->respondWithToken(Auth::guard('api')->refresh());
+    }
+
+    protected function respondWithToken(string $token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => Auth::guard('api')->factory()->getTTL() * 60,
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,15 @@
 <?php
 
 use App\Http\Controllers\VinaController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+
+Route::prefix('auth')->group(function () {
+	Route::post('/login', [AuthController::class, 'login']);
+	Route::get('/me', [AuthController::class, 'me']);
+	Route::post('/logout', [AuthController::class, 'logout']);
+	Route::post('/refresh', [AuthController::class, 'refresh']);
+});
 
 Route::get('/vina-2026/parrilla', [VinaController::class, 'getParrilla']);
 Route::get('/vina-2026/dia/{dia}', [VinaController::class, 'getDia']);
